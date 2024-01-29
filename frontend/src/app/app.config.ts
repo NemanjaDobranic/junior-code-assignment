108ab9aa9a538fad2da9401a574f28d2a8e0c803
NemanjaDobranic <dobranicnemanja2014@gmail.com>
2024-01-29 09:42:25 +0100

    added config for jwt and http client services

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+  ],
 };
